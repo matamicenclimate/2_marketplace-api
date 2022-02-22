52e8fe0a2009c57ea39234d0468bf866d6e7a6db
NandawteK <nandawtek@gmail.com>
2022-02-22 13:26:14 +0100

    fix cors

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,8 +1,8 @@
 import Koa from 'koa'
 export async function cors(ctx: Koa.Context, next: Koa.Next) {
-  ctx.res.setHeader('Access-Control-Allow-Origin', '*')
-  ctx.res.setHeader('Access-Control-Allow-Methods', '*')
-  ctx.res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  ctx.set('Access-Control-Allow-Origin', '*')
+  ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
   await next()
 }
 

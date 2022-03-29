24e8621d1339508ad68600bb9745ec0dd24eb8be
pblanco-dekalabs <pblanco@dekalabs.com>
2022-03-29 13:42:05 +0200

    Fixed AVM provider

--- src/services/impl/FSAVMProgramProvider.ts
+++ src/services/impl/FSAVMProgramProvider.ts
@@ -1,22 +1,30 @@
-import AlgodClientProvider from '@common/services/AlgodClientProvider';
+import AlgodClientProvider from '@common/services/AlgodClientProvider'
 import * as AVMProgramProvider from '@common/services/AVMProgramProvider'
-import * as fs from 'fs/promises';
-import { Inject } from 'typedi';
+import * as fs from 'fs/promises'
+import { Inject } from 'typedi'
 
 @AVMProgramProvider.declare()
 export default class FSAVMProgramProvider implements AVMProgramProvider.type {
   @Inject()
   private readonly algoClientProvider: AlgodClientProvider
   get auctionApprovalProgram(): Promise<Uint8Array> {
-    return fs.readFile('./contracts/auction_approval.teal',).then(async (buffer) => {
-      const program: string = await this.algoClientProvider.client.compile(buffer.toString()).do()
-      return Buffer.from(program, 'base64')
-    })
+    return fs
+      .readFile('./contracts/auction_approval.teal')
+      .then(async buffer => {
+        const program: { result: string } = await this.algoClientProvider.client
+          .compile(buffer.toString())
+          .do()
+        return new Uint8Array(Buffer.from(program.result, 'base64'))
+      })
   }
   get clearStateProgram(): Promise<Uint8Array> {
-    return fs.readFile('./contracts/auction_clear_state.teal',).then(async (buffer) => {
-      const program: string = await this.algoClientProvider.client.compile(buffer.toString()).do()
-      return Buffer.from(program, 'base64')
-    })
+    return fs
+      .readFile('./contracts/auction_clear_state.teal')
+      .then(async buffer => {
+        const program: { result: string } = await this.algoClientProvider.client
+          .compile(buffer.toString())
+          .do()
+        return new Uint8Array(Buffer.from(program.result, 'base64'))
+      })
   }
 }

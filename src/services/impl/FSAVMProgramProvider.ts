e3e5862f100b7f096ac08216b6b475c9f99a5d44
NandawteK <nandawtek@gmail.com>
2022-03-29 11:22:32 +0200

    fix import teals

--- src/services/impl/FSAVMProgramProvider.ts
+++ src/services/impl/FSAVMProgramProvider.ts
@@ -1,12 +1,22 @@
+import AlgodClientProvider from '@common/services/AlgodClientProvider';
 import * as AVMProgramProvider from '@common/services/AVMProgramProvider'
 import * as fs from 'fs/promises';
+import { Inject } from 'typedi';
 
 @AVMProgramProvider.declare()
 export default class FSAVMProgramProvider implements AVMProgramProvider.type {
+  @Inject()
+  private readonly algoClientProvider: AlgodClientProvider
   get auctionApprovalProgram(): Promise<Uint8Array> {
-    return fs.readFile('./contracts/auction_approval.teal',).then((buffer) => new Uint8Array(buffer))
+    return fs.readFile('./contracts/auction_approval.teal',).then(async (buffer) => {
+      const program: string = await this.algoClientProvider.client.compile(buffer).do()
+      return Buffer.from(program, 'base64')
+    })
   }
   get clearStateProgram(): Promise<Uint8Array> {
-    return fs.readFile('./contracts/auction_clear_state.teal',).then((buffer) => new Uint8Array(buffer))
+    return fs.readFile('./contracts/auction_clear_state.teal',).then(async (buffer) => {
+      const program: string = await this.algoClientProvider.client.compile(buffer).do()
+      return Buffer.from(program, 'base64')
+    })
   }
 }

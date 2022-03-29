2e543f4e726a57d75d4fcffb3459ef5f1be3d160
NandawteK <nandawtek@gmail.com>
2022-03-29 12:06:50 +0200

    fix buffer as string

--- src/services/impl/FSAVMProgramProvider.ts
+++ src/services/impl/FSAVMProgramProvider.ts
@@ -9,13 +9,13 @@ export default class FSAVMProgramProvider implements AVMProgramProvider.type {
   private readonly algoClientProvider: AlgodClientProvider
   get auctionApprovalProgram(): Promise<Uint8Array> {
     return fs.readFile('./contracts/auction_approval.teal',).then(async (buffer) => {
-      const program: string = await this.algoClientProvider.client.compile(buffer).do()
+      const program: string = await this.algoClientProvider.client.compile(buffer.toString()).do()
       return Buffer.from(program, 'base64')
     })
   }
   get clearStateProgram(): Promise<Uint8Array> {
     return fs.readFile('./contracts/auction_clear_state.teal',).then(async (buffer) => {
-      const program: string = await this.algoClientProvider.client.compile(buffer).do()
+      const program: string = await this.algoClientProvider.client.compile(buffer.toString()).do()
       return Buffer.from(program, 'base64')
     })
   }

5b5817f365876b901c4df2989ceb4063807d7dbf
NandawteK <nandawtek@gmail.com>
2022-05-20 12:35:28 +0200

    retry create auction when fails

--- src/controllers/MintController.ts
+++ src/controllers/MintController.ts
@@ -1,10 +1,11 @@
-import { Body, JsonController, Param, Post } from 'routing-controllers'
-import Container, { Inject, Service } from 'typedi'
+import { Body, JsonController, Post } from 'routing-controllers'
+import { Inject, Service } from 'typedi'
 import OptInService from '@common/services/OptInService'
 import * as WalletAccountProvider from '@common/services/WalletAccountProvider'
 import AuctionService from '../services/AuctionService'
 import ListingService from 'src/services/ListingService'
-import DbConnectionService from 'src/services/DbConnectionService'
+import UnsignedTransactionService from 'src/services/UnsignedTransactionService'
+import TransactionGroupService from 'src/services/TransactionGroupService'
 import config from '../config/default'
 import { OpenAPI } from 'routing-controllers-openapi'
 import CustomLogger from 'src/infrastructure/CustomLogger'
@@ -13,6 +14,8 @@ import { AssetNormalized } from 'src/interfaces'
 import { option } from '@octantis/option'
 import { Response, Body as BodyCommon } from '@common/lib/api'
 import { core } from '@common/lib/api/endpoints'
+import DbConnectionService from 'src/services/DbConnectionService'
+import algosdk from 'algosdk'
 
 @Service()
 @JsonController('/api')
@@ -22,6 +25,12 @@ export default class MintController {
 
   @Inject()
   readonly auctionService: AuctionService
+
+  @Inject()
+  readonly unsignedtransactionService: UnsignedTransactionService 
+
+  @Inject()
+  readonly transactionGroupService: TransactionGroupService 
 
   @Inject()
   readonly listingService: ListingService
@@ -43,34 +52,28 @@ export default class MintController {
       endDate
     }: BodyCommon<core['post']['create-auction']>
   ): Promise<Response<core['post']['create-auction']>> {
-    try {
-      const populatedAsset = await this.listingService.populateAsset(assetId)
-      const asset: option<AssetNormalized> =
-        await this.listingService.normalizeAsset(populatedAsset)
-      if (asset.isDefined()) {
-        const db = await DbConnectionService.create()
-        const response = await this.auctionService.execute(
-          assetId,
-          asset.value,
-          creatorWallet,
-          causePercentage,
-          startDate,
-          endDate,
-          db
-        )
-        this.logger.info(
-          `DONE: Sending back the asset ${assetId} to wallet owner.`
-        )
-        return response
-      } else {
-        throw new ServiceException(
-          `Create auction error: Asset ${assetId} not found`
-        )
+    let attemps = 0
+    while(true) {
+      try {
+        return await this.tryCreateAuction(assetId, creatorWallet, causePercentage, startDate, endDate)
+      } catch (error) {
+        const message = `Create auction error: ${error.message}`
+        this.logger.error(message, { stack: error.stack })
+        if (attemps >= 3) {
+          const optOutTxUnsigned = await this.optInService.createOptInRequest(
+            assetId,
+            this.wallet.account.addr,
+            creatorWallet,
+            1)
+          const depositTxUnsigned = await this.unsignedtransactionService.execute(this.wallet.account.addr, creatorWallet, this._getDepositAmount())
+          
+          const txId = await this.transactionGroupService.execute([optOutTxUnsigned, depositTxUnsigned])
+          this.logger.info(`Asset ${assetId} is returned to creator and deposit too with transaction ${txId}`)
+  
+          throw new ServiceException(message)
+        }
+        attemps++
       }
-    } catch (error) {
-      const message = `Create auction error: ${error.message}`
-      this.logger.error(message, { stack: error.stack })
-      throw new ServiceException(message)
     }
   }
 
@@ -105,4 +108,40 @@ export default class MintController {
       throw new ServiceException(message)
     }
   }
+  async tryCreateAuction (
+    assetId: number,
+    creatorWallet: string,
+    causePercentage: number,
+    startDate: string,
+    endDate: string
+  ) {
+    const populatedAsset = await this.listingService.populateAsset(assetId)
+    const asset: option<AssetNormalized> = await this.listingService.normalizeAsset(populatedAsset)
+    if (asset.isDefined()) {
+      const db = await DbConnectionService.create()
+      const response = await this.auctionService.execute(
+        assetId,
+        asset.value,
+        creatorWallet,
+        causePercentage,
+        startDate,
+        endDate,
+        db
+      )
+      this.logger.info(
+        `DONE: Sending back the asset ${assetId} to wallet owner.`
+      )
+      return response
+    } else {
+      throw new ServiceException(`Create auction error: Asset ${assetId} not found`)
+    }
+  }
+
+  _getDepositAmount() {
+    const RETURN_ASSET_TO_CREATOR_FEE = 1
+    const DEPOSIT_TRANSACTION_FEE = 1
+    const DEPOSIT = 4 - RETURN_ASSET_TO_CREATOR_FEE - DEPOSIT_TRANSACTION_FEE
+    return DEPOSIT * algosdk.ALGORAND_MIN_TX_FEE
+  }
 }
+
---
+++ src/services/TransactionGroupService.ts
@@ -0,0 +1,29 @@
+import AlgodClientProvider from '@common/services/AlgodClientProvider'
+import Container, { Inject, Service } from 'typedi'
+import algosdk, { TransactionLike } from 'algosdk'
+import CustomLogger from 'src/infrastructure/CustomLogger'
+import * as TransactionSigner from '@common/services/TransactionSigner'
+
+@Service()
+export default class TransactionGroupService {
+  readonly client: AlgodClientProvider
+  @Inject()
+  private readonly logger!: CustomLogger
+  @TransactionSigner.inject() 
+  readonly signer: TransactionSigner.type
+
+  constructor() {
+    this.client = Container.get(AlgodClientProvider)
+  }
+
+  async execute(transactions: TransactionLike[]) {
+    this.logger.info('Grouping transactions', {
+      transactions
+    })
+    const txns = algosdk.assignGroupID(transactions)
+    const signedTxn = await this.signer.signTransaction(txns)
+    const { txId } = await this.client.client.sendRawTransaction(signedTxn).do()
+
+    return txId
+  }
+}
